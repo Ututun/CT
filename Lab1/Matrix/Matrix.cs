bd4b23fda379506626141a20b10547b6d20a5e93
darknyaz <darknyaz@yandex.ru>
2014-02-12 23:22:24 +0400

    Operator%

---
+++ Lab1/Matrix/Matrix.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+
+namespace LinearAlgebra
+{
+	public class IncorrectMatrixSizes : Exception { }
+
+    public class Matrix
+    {
+		public int RowNumber { get; private set; }
+		public int ColumnNumber { get; private set; }
+		int[,] _matrix;
+
+		public Matrix(int rowNumber, int columnNumber)
+		{
+			RowNumber = rowNumber;
+			ColumnNumber = columnNumber;
+			_matrix = new int[RowNumber, ColumnNumber];
+		}
+
+        public static Matrix operator+(Matrix leftHandSide, Matrix rightHandSide)
+        {
+			if (leftHandSide.RowNumber != rightHandSide.RowNumber || leftHandSide.ColumnNumber != rightHandSide.ColumnNumber)
+				throw new IncorrectMatrixSizes();
+
+            var result = new Matrix(leftHandSide.RowNumber, rightHandSide.ColumnNumber);
+
+            for (int i = 0; i < leftHandSide.RowNumber; ++i)
+                for (int j = 0; j < rightHandSide.ColumnNumber; ++j)
+                    result[i, j] = leftHandSide[i, j] + rightHandSide[i, j];
+
+            return result;
+        }
+
+		public static Matrix operator*(Matrix leftHandSide, Matrix rightHandSide)
+		{
+			if (leftHandSide.ColumnNumber != rightHandSide.RowNumber)
+				throw new IncorrectMatrixSizes();
+
+			var result = new Matrix(leftHandSide.RowNumber, rightHandSide.ColumnNumber);
+
+			for (int i = 0; i < leftHandSide.RowNumber; ++i)
+				for (int j = 0; j < rightHandSide.ColumnNumber; ++j)
+					for (int k = 0; k < leftHandSide.ColumnNumber; ++k)
+						result[i, j] += leftHandSide[i, k] * rightHandSide[k, j];
+
+			return result;
+		}
+
+		// Marina's task
+		//public static Matrix operator%(Matrix matrix, int module)
+		//{
+			
+		//}
+
+		public int this[int rowIndex, int columnIndex]
+		{
+			get { return _matrix[rowIndex, columnIndex]; }
+			set { _matrix[rowIndex, columnIndex] = value; }
+		}
+	}
+}
--- Lab1/ModularMartixTests/Tests.cs
+++ Lab1/ModularMartixTests/Tests.cs
@@ -7,15 +7,24 @@ namespace ModularMartixTests
 	[TestClass]
 	public class Tests
 	{
+		[TestMethod, ExpectedException(typeof(IncorrectMatrixSizes))]
+		public void IncorrectMatrixSizesExceptionInMultiplication()
+		{
+			var A = new Matrix(2, 3);
+			var B = new Matrix(2, 2);
+
+			var C = A * B;
+		}
+
 		[TestMethod]
 		public void MultiplicationTest1()
 		{
-			var A = new ModularMatrix(2, 2);
+			var A = new Matrix(2, 2);
 			A[0, 0] = 1;
 			A[0, 1] = 2;
 			A[1, 0] = 3;
 			A[1, 1] = 4;
-			var B = new ModularMatrix(2, 2);
+			var B = new Matrix(2, 2);
 			B[0, 0] = 1;
 			B[0, 1] = 2;
 			B[1, 0] = 3;
@@ -32,14 +41,14 @@ namespace ModularMartixTests
 		[TestMethod]
 		public void MultiplicationTest2()
 		{
-			var A = new ModularMatrix(2, 3);
+			var A = new Matrix(2, 3);
 			A[0, 0] = -1;
 			A[0, 1] = 0;
 			A[0, 2] = 1;
 			A[1, 0] = 6;
 			A[1, 1] = 0;
 			A[1, 2] = 9;
-			var B = new ModularMatrix(3, 2);
+			var B = new Matrix(3, 2);
 			B[0, 0] = -6;
 			B[0, 1] = 6;
 			B[1, 0] = 9;
@@ -58,12 +67,12 @@ namespace ModularMartixTests
         [TestMethod]
         public void AdditionTest1()
         {
-            var A = new ModularMatrix(2, 2);
+            var A = new Matrix(2, 2);
             A[0, 0] = 1;
             A[0, 1] = 2;
             A[1, 0] = 3;
             A[1, 1] = 4;
-            var B = new ModularMatrix(2, 2);
+            var B = new Matrix(2, 2);
             B[0, 0] = 1;
             B[0, 1] = 2;
             B[1, 0] = 3;
