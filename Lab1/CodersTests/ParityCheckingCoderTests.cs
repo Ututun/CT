d0f869bf5c33a368181002ff82491b2a0c7b61f9
Ututun <Ututun@yandex.ru>
2014-02-20 18:37:07 +0300

    Tests have become more beautiful

--- Lab1/CodersTests/ParityCheckingCoderTests.cs
+++ Lab1/CodersTests/ParityCheckingCoderTests.cs
@@ -12,12 +12,7 @@ namespace CodersTests
 		public void IncorrectInformationRowVectorLengthExceptionInEncodeTest1()
 		{
 			var coder = new ParityCheckingCoder(4);
-			var cipher = new RowVector(5);
-			cipher[0] = 1;
-			cipher[1] = 0;
-			cipher[2] = 0;
-			cipher[3] = 1;
-			cipher[4] = 1;
+            var cipher = new RowVector { 1, 0, 0, 1, 1 };
 
 			var result = coder.Decode(cipher);
 		}
@@ -26,15 +21,7 @@ namespace CodersTests
         public void IncorrectInformationRowVectorLengthExceptionInEncodeTest2()
         {
             var coder = new ParityCheckingCoder(7);
-            var cipher = new RowVector(8);
-            cipher[0] = 1;
-            cipher[1] = 0;
-            cipher[2] = 0;
-            cipher[3] = 1;
-            cipher[4] = 1;
-            cipher[5] = 1;
-            cipher[6] = 0;
-            cipher[7] = 1;
+            var cipher = new RowVector { 1, 0, 0, 1, 1, 1, 0, 1 };
 
             var result = coder.Decode(cipher);
         }
@@ -43,104 +30,55 @@ namespace CodersTests
 		public void EncodeTest1()
 		{
 			var coder = new ParityCheckingCoder(4);
-			var information = new RowVector(4);
-			information[0] = 1;
-			information[1] = 0;
-			information[2] = 0;
-			information[3] = 1;
+            var information = new RowVector { 1, 0, 0, 1 };
 
 			var result = coder.Encode(information);
 
-			Assert.AreEqual(result[0], 1);
-			Assert.AreEqual(result[1], 0);
-			Assert.AreEqual(result[2], 0);
-			Assert.AreEqual(result[3], 1);
-			Assert.AreEqual(result[4], 0);
+            CollectionAssert.AreEqual(result, new[] { 1, 0, 0, 1, 0 });
 		}
 
         [TestMethod]
         public void EncodeTest2()
         {
             var coder = new ParityCheckingCoder(3);
-            var information = new RowVector(3);
-            information[0] = 1;
-            information[1] = 0;
-            information[2] = 1;
+            var information = new RowVector { 1, 0, 1 };
 
             var result = coder.Encode(information);
 
-            Assert.AreEqual(result[0], 1);
-            Assert.AreEqual(result[1], 0);
-            Assert.AreEqual(result[2], 1);
-            Assert.AreEqual(result[3], 0);
+            CollectionAssert.AreEqual(result, new[] { 1, 0, 1, 0 });
         }
 
         [TestMethod]
         public void EncodeTest3()
         {
             var coder = new ParityCheckingCoder(10);
-            var information = new RowVector(10);
-            information[0] = 1;
-            information[1] = 1;
-            information[2] = 0;
-            information[3] = 0;
-            information[4] = 1;
-            information[5] = 0;
-            information[6] = 0;
-            information[7] = 1;
-            information[8] = 0;
-            information[9] = 1;
+            var information = new RowVector { 1, 1, 0, 0, 1, 0, 0, 1, 0, 1 };
 
             var result = coder.Encode(information);
 
-            Assert.AreEqual(result[0], 1);
-            Assert.AreEqual(result[1], 1);
-            Assert.AreEqual(result[2], 0);
-            Assert.AreEqual(result[3], 0);
-            Assert.AreEqual(result[4], 1);
-            Assert.AreEqual(result[5], 0);
-            Assert.AreEqual(result[6], 0);
-            Assert.AreEqual(result[7], 1);
-            Assert.AreEqual(result[8], 0);
-            Assert.AreEqual(result[9], 1);
-            Assert.AreEqual(result[10], 1);
+            CollectionAssert.AreEqual(result, new[] { 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1 });
         }
 
 		[TestMethod]
 		public void DecodeTest1()
 		{
 			var coder = new ParityCheckingCoder(4);
-			var cipher = new RowVector(5);
-			cipher[0] = 1;
-			cipher[1] = 0;
-			cipher[2] = 0;
-			cipher[3] = 1;
-			cipher[4] = 0;
+            var cipher = new RowVector { 1, 0, 0, 1, 0 };
 
 			var result = coder.Decode(cipher);
 
-			Assert.AreEqual(result[0], 1);
-			Assert.AreEqual(result[1], 0);
-			Assert.AreEqual(result[2], 0);
-			Assert.AreEqual(result[3], 1);
+            CollectionAssert.AreEqual(result, new[] { 1, 0, 0, 1 });
 		}
 
         [TestMethod]
         public void DecodeTest2()
         {
             var coder = new ParityCheckingCoder(7);
-            var cipher = new RowVector(8);
-            cipher[0] = 1;
-            cipher[1] = 0;
-            cipher[2] = 1;
-            cipher[3] = 1;
-            cipher[4] = 1;
-            cipher[5] = 1;
-            cipher[6] = 0;
-            cipher[7] = 1;
+            var cipher = new RowVector { 1, 0, 1, 1, 1, 1, 0, 1 };
 
             var result = coder.Decode(cipher);
 
+            CollectionAssert.AreEqual(result, new[] { 1, 0, 1, 1, 1, 1, 0 });
             Assert.AreEqual(result[0], 1);
             Assert.AreEqual(result[1], 0);
             Assert.AreEqual(result[2], 1);
@@ -154,31 +92,11 @@ namespace CodersTests
         public void DecodeTest3()
         {
             var coder = new ParityCheckingCoder(10);
-            var cipher = new RowVector(11);
-            cipher[0] = 1;
-            cipher[1] = 0;
-            cipher[2] = 1;
-            cipher[3] = 1;
-            cipher[4] = 1;
-            cipher[5] = 1;
-            cipher[6] = 0;
-            cipher[7] = 1;
-            cipher[8] = 1;
-            cipher[9] = 0;
-            cipher[10] = 1;
+            var cipher = new RowVector { 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1 };
 
             var result = coder.Decode(cipher);
 
-            Assert.AreEqual(result[0], 1);
-            Assert.AreEqual(result[1], 0);
-            Assert.AreEqual(result[2], 1);
-            Assert.AreEqual(result[3], 1);
-            Assert.AreEqual(result[4], 1);
-            Assert.AreEqual(result[5], 1);
-            Assert.AreEqual(result[6], 0);
-            Assert.AreEqual(result[7], 1);
-            Assert.AreEqual(result[8], 1);
-            Assert.AreEqual(result[9], 0);
+            CollectionAssert.AreEqual(result, new[] { 1, 0, 1, 1, 1, 1, 0, 1, 1, 0 });
         }
     }
 }
--- Lab1/CodersTests/RepeatCoderTests.cs
+++ Lab1/CodersTests/RepeatCoderTests.cs
@@ -12,10 +12,7 @@ namespace CodersTests
 		public void IncorrectInformationRowVectorLengthExceptionInEncodeTest1()
 		{
 			var coder = new RepeatCoder(4, 1);
-			var information = new RowVector(3);
-			information[0] = 1;
-			information[1] = 0;
-			information[2] = 0;
+            var information = new RowVector { 1, 0, 0 };
 
 			var result = coder.Encode(information);
 		}
@@ -24,10 +21,7 @@ namespace CodersTests
         public void IncorrectInformationRowVectorLengthExceptionInEncodeTest2()
         {
             var coder = new RepeatCoder(5, 2);
-            var information = new RowVector(3);
-            information[0] = 1;
-            information[1] = 1;
-            information[2] = 0;
+            var information = new RowVector { 1, 1, 0 };
 
             var result = coder.Encode(information);
         }
@@ -36,154 +30,66 @@ namespace CodersTests
 		public void EncodeTest1()
 		{
 			var coder = new RepeatCoder(4, 1);
-			var information = new RowVector(4);
-			information[0] = 1; 
-			information[1] = 0;
-			information[2] = 0;
-			information[3] = 1;
+            var information = new RowVector { 1, 0, 0, 1 };
 
 			var result = coder.Encode(information);
 
-			Assert.AreEqual(result[0], 1);
-			Assert.AreEqual(result[1], 0);
-			Assert.AreEqual(result[2], 0);
-			Assert.AreEqual(result[3], 1);
-			Assert.AreEqual(result[4], 1);
-			Assert.AreEqual(result[5], 0);
-			Assert.AreEqual(result[6], 0);
-			Assert.AreEqual(result[7], 1);
-			Assert.AreEqual(result[8], 1);
-			Assert.AreEqual(result[9], 0);
-			Assert.AreEqual(result[10], 0);
-			Assert.AreEqual(result[11], 1);
+            CollectionAssert.AreEqual(result, new[] { 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1 });
 		}
 
         [TestMethod]
         public void EncodeTest2()
         {
             var coder = new RepeatCoder(2, 2);
-            var information = new RowVector(2);
-            information[0] = 1;
-            information[1] = 0;
+            var information = new RowVector { 1, 0 };
 
             var result = coder.Encode(information);
 
-            Assert.AreEqual(result[0], 1);
-            Assert.AreEqual(result[1], 0);
-            Assert.AreEqual(result[2], 1);
-            Assert.AreEqual(result[3], 0);
-            Assert.AreEqual(result[4], 1);
-            Assert.AreEqual(result[5], 0);
-            Assert.AreEqual(result[6], 1);
-            Assert.AreEqual(result[7], 0);
-            Assert.AreEqual(result[8], 1);
-            Assert.AreEqual(result[9], 0);
+            CollectionAssert.AreEqual(result, new[] { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0 });
         }
 
         [TestMethod]
         public void EncodeTest3()
         {
             var coder = new RepeatCoder(2, 3);
-            var information = new RowVector(2);
-            information[0] = 1;
-            information[1] = 0;
+            var information = new RowVector { 1, 0 };
 
             var result = coder.Encode(information);
 
-            Assert.AreEqual(result[0], 1);
-            Assert.AreEqual(result[1], 0);
-            Assert.AreEqual(result[2], 1);
-            Assert.AreEqual(result[3], 0);
-            Assert.AreEqual(result[4], 1);
-            Assert.AreEqual(result[5], 0);
-            Assert.AreEqual(result[6], 1);
-            Assert.AreEqual(result[7], 0);
-            Assert.AreEqual(result[8], 1);
-            Assert.AreEqual(result[9], 0);
-            Assert.AreEqual(result[10], 1);
-            Assert.AreEqual(result[11], 0);
-            Assert.AreEqual(result[12], 1);
-            Assert.AreEqual(result[13], 0);
+            CollectionAssert.AreEqual(result, new[] { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0 });
         }
 
 		[TestMethod]
 		public void DecodeTest1()
 		{
 			var coder = new RepeatCoder(4, 1);
-			var cipher = new RowVector(12);
-			cipher[0] = 1;
-			cipher[1] = 0;
-			cipher[2] = 0;
-			cipher[3] = 1;
-			cipher[4] = 0;
-			cipher[5] = 0;
-			cipher[6] = 0;
-			cipher[7] = 1;
-			cipher[8] = 1;
-			cipher[9] = 0;
-			cipher[10] = 0;
-			cipher[11] = 1;
+			var cipher = new RowVector { 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1 };
 
 			var result = coder.Decode(cipher);
 
-			Assert.AreEqual(result[0], 1);
-			Assert.AreEqual(result[1], 0);
-			Assert.AreEqual(result[2], 0);
-			Assert.AreEqual(result[3], 1);
+            CollectionAssert.AreEqual(result, new[] { 1, 0, 0, 1 });
 		}
 
         [TestMethod]
         public void DecodeTest2()
         {
             var coder = new RepeatCoder(3, 2);
-            var cipher = new RowVector(15);
-            cipher[0] = 1;
-            cipher[1] = 0;
-            cipher[2] = 1;
-            cipher[3] = 1;
-            cipher[4] = 0;
-            cipher[5] = 0;
-            cipher[6] = 1;
-            cipher[7] = 1;
-            cipher[8] = 0;
-            cipher[9] = 1;
-            cipher[10] = 0;
-            cipher[11] = 0;
-            cipher[12] = 1;
-            cipher[13] = 0;
-            cipher[14] = 0;
+            var cipher = new RowVector { 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0 };
 
             var result = coder.Decode(cipher);
 
-            Assert.AreEqual(result[0], 1);
-            Assert.AreEqual(result[1], 0);
-            Assert.AreEqual(result[2], 0);
+            CollectionAssert.AreEqual(result, new[] { 1, 0, 0 });
         }
 
         [TestMethod]
         public void DecodeTest3()
         {
             var coder = new RepeatCoder(2, 3);
-            var cipher = new RowVector(14);
-            cipher[0] = 1;
-            cipher[1] = 1;
-            cipher[2] = 1;
-            cipher[3] = 0;
-            cipher[4] = 1;
-            cipher[5] = 1;
-            cipher[6] = 1;
-            cipher[7] = 0;
-            cipher[8] = 1;
-            cipher[9] = 0;
-            cipher[10] = 0;
-            cipher[11] = 0;
-            cipher[12] = 1;
-            cipher[13] = 0;
+            var cipher = new RowVector { 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0 };
 
             var result = coder.Decode(cipher);
 
-            Assert.AreEqual(result[0], 1);
-            Assert.AreEqual(result[1], 0);
+            CollectionAssert.AreEqual(result, new[] { 1, 0 });
         }
 	}
 }
